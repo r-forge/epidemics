6eb37fc674449e5ae8ab72003d3a8a77a883cff9
jombart (Mr ) <t.jombart@imperial.ac.uk>
2011-10-26 11:16:36 +0000

    Added new skeletons for future files.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/epidemics@40 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/src/sumstat.c
@@ -0,0 +1,35 @@
+/*
+  Coded by Thibaut Jombart (t.jombart@imperial.ac.uk), September 2011.
+  Distributed with the epidemics package for the R software.
+  Licence: GPL >=2.
+
+  These functions are basic routines for simulating sequence evolution.
+*/
+
+#include "common.h"
+#include "param.h"
+#include "pathogens.h"
+#include "populations.h"
+
+
+
+
+/*
+   ===========================
+   === AUXILIARY FUNCTIONS ===
+   ===========================
+*/
+
+
+
+
+
+
+
+/*
+   ===============================
+   === MAIN EXTERNAL FUNCTIONS ===
+   ===============================
+*/
+
+
---
+++ pkg/src/sumstat.h
@@ -0,0 +1,24 @@
+/*
+  Coded by Thibaut Jombart (t.jombart@imperial.ac.uk), September 2011.
+  Distributed with the epidemics package for the R software.
+  Licence: GPL >=2.
+
+  These functions are basic routines for simulating host populations.
+*/
+
+
+
+
+/*
+   ===========================
+   === AUXILIARY FUNCTIONS ===
+   ===========================
+*/
+
+
+/*
+   ==========================
+   === EXTERNAL FUNCTIONS ===
+   ==========================
+*/
+
