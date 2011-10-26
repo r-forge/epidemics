110a8fdf0940e210b86b9e7af0771480f752f95f
jombart (Mr ) <t.jombart@imperial.ac.uk>
2011-10-26 14:30:51 +0000

    first draft of function listing all snps in a sample
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/epidemics@43 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/src/sumstat.c
+++ pkg/src/sumstat.c
@@ -15,6 +15,49 @@
 
 
 /*
+   ====================
+   === CONSTRUCTORS ===
+   ====================
+*/
+
+struct snplist * create_snplist(int n){
+	out = calloc(1, sizeof(snplist));
+	if(out == NULL){
+		fprintf(stderr, "\n[in: sumstat.c->create_snplist]\nNo memory left for listing SNPs. Exiting.\n");
+		exit(1);
+	}
+
+	out->snps = calloc(n, sizeof(int));
+	if(out->snps == NULL){
+		fprintf(stderr, "\n[in: sumstat.c->create_snplist]\nNo memory left for listing SNPs. Exiting.\n");
+		exit(1);
+	}
+
+	out->length = n;
+
+	return out;
+}
+
+
+
+
+
+/*
+   ===================
+   === DESTRUCTORS ===
+   ===================
+*/
+
+void free_snplist(struct snplist *in){
+	if(in->snps != NULL) free(in->snps);
+	if(in != NULL) free(in);
+}
+
+
+
+
+
+/*
    ===========================
    === AUXILIARY FUNCTIONS ===
    ===========================
@@ -22,50 +65,63 @@
 
 /* check if an integer is in a vector of integers, and return the matching position */
 int int_in_vec(int x, int *vec, int vecSize){
-	int i=0; 
+	int i=0;
 	while(x!=vec[i] && i<vecSize) i++;
 	if(i==vecSize || vecSize<1) return -1; /* -1 will mean: no match*/
 	return i;
 }
 
 
+
 /* count number of snps in a sample */
 struct snplist * list_snps(struct sample *in, struct param *par){
-	int i=0, j=0, N=get_n(in), *pool, poolsize=0, curNbSnps;
-
-	/* allocate output */
+	int i=0, j=0, N=get_n(in), *pool, poolsize, curNbSnps;
 	struct snplist *out;
-	out = calloc(1, sizeof(snplist));
-	if(out == NULL){
-		fprintf(stderr, "\n[in: sumstat.c->list_snps]\nNo memory left for listing SNPs. Exiting.\n");
-		exit(1);
-	}
 
 	/* create pool of snps */
 	pool = calloc(par->L, sizeof(int));
+	if(pool == NULL){
+		fprintf(stderr, "\n[in: sumstat.c->list_snps]\nNo memory left for creating pool of SNPs. Exiting.\n");
+		exit(1);
+	}
+
+	/* list and count all SNPs */
+	poolsize = 0;
 	for(i=0;i<N;i++){
 		curNbSnps = get_nb_snps(in->pathogens[i]);
-		for(j=0;j<poolsize;j++){
-			if(int_in_vec(pool[j], get_snps(in->pathogens[i]), curNbSnps)){
-				pool[poolsize] =
+		for(j=0;j<curNbSnps;j++){
+			if(int_in_vec(get_snps(in->pathogens[i])[j], pool, poolsize) < 0){
+				pool[poolsize++] = get_snps(in->pathogens[i])[j];
 			}
 		}
 	}
 
+	/* make output */
+	out = create_snplist(poolsize);
+
+	for(i=0;i<poolsize;i++) out->snps[i] = pool[i];
+
+	/* free local pointers and return result */
+	free(pool);
 	return out;
 }
 
 
-void free_snplist(struct snplist *in){
-	if(in->snps != NULL) free(in->snps);
-	if(in != NULL) free(in);
-}
+
 
 /*
    ===============================
    === MAIN EXTERNAL FUNCTIONS ===
    ===============================
 */
+
+void print_snplist(struct snplist *in){
+	int i;
+	printf("\nlist of %d SNPs:\n", in->length);
+	for(i=0;i<in->length;i++) printf("%d ",in->snps[i]);
+	printf("\n");
+}
+
 
 /* double hs(struct sample *in, struct param *par){ */
 /* 	int i; */
