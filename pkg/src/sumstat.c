2318a81c029b35177e3721440458f9add4925336
jombart (Mr ) <t.jombart@imperial.ac.uk>
2011-10-26 13:36:31 +0000

    %in% for integers works
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/epidemics@41 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/src/sumstat.c
+++ pkg/src/sumstat.c
@@ -20,6 +20,13 @@
    ===========================
 */
 
+/* check if an integer is in a vector of integers */
+bool int_in_vec(int x, int *vec, int vecSize){
+	int i=0;
+	while(x!=vec[i] && i<vecSize) i++;
+	if(i==vecSize || vecSize<1) return FALSE;
+	return TRUE;
+}
 
 
 
@@ -32,4 +39,26 @@
    ===============================
 */
 
+/* double hs(struct sample *in, struct param *par){ */
+/* 	int i; */
+/* 	double out; */
 
+/* 	return out; */
+/* 	/\* *\/ */
+/* } */
+
+
+
+
+/*
+   =========================
+   === TESTING FUNCTIONS ===
+   =========================
+*/
+int main(){
+	int  i, vec[5]={1,2,3,4,5};
+
+	for(i=0;i<10;i++) printf("\ni=%d, result:%d", i, int_in_vec(i,vec,5));
+	
+	return 0;
+}
