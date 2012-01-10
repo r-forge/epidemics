4282d6c9092a9c29e3616a9ed33406681dbbaaa3
jombart (Mr ) <t.jombart@imperial.ac.uk>
2012-01-10 19:00:46 +0000

    trial code
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/epidemics@186 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ misc/parallel.c
@@ -0,0 +1,135 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <omp.h>
+
+int main(int argc, char *argv[]){
+/* #pragma omp parallel */
+/* 	{ */
+/* 		printf("Hello, world.\n"); */
+/* 	} */
+
+/* 	const int N = 100000; */
+/* 	int i, a[N], th_id, nthreads; */
+
+/* #pragma omp parallel private(i) */
+/* 	{ */
+/* 		printf("\nthread number: %d \n", omp_get_thread_num()); */
+/* 	} */
+
+/* 	omp_set_num_threads(5); */
+
+/* #pragma omp parallel for */
+/* 	for (i = 0; i < N; i++){ */
+/* 		a[i] = 2 * i; */
+/* 	} */
+
+/* #pragma omp parallel private(th_id) */
+/* 	{ */
+/* 		th_id = omp_get_thread_num(); */
+/* 		printf("Hello World from thread %d\n", th_id); */
+/* #pragma omp barrier */
+/* 		if ( th_id == 0 ) { */
+/* 			nthreads = omp_get_num_threads(); */
+/* 			printf("There are %d threads\n",nthreads); */
+/* 		} */
+/* 	} */
+	
+/* 	printf("\ntest conditional compiling (yes):\n"); */
+/* #define USE_OMP 1 */
+/* #if USE_OMP */
+/* #pragma omp parallel */
+/* #endif */
+/* 	{ */
+/* 		printf("Hello from thread %d\n", omp_get_thread_num()); */
+/* 	} */
+	
+/* 	printf("\ntest conditional compiling (no):\n"); */
+/* #ifdef USE_OMP */
+/* #undef USE_OMP */
+/* #endif */
+/* #define USE_OMP 0 */
+/* #if USE_OMP */
+/* #pragma omp parallel */
+/* #endif */
+/* 	{ */
+/* 		printf("Hello from thread %d\n", omp_get_thread_num()); */
+/* 	} */
+
+
+
+/* 	printf("\ntest for loop:\n"); */
+
+/* /\* #undef USE_OMP *\/ */
+/* /\* #define USE_OMP 1 *\/ */
+/* /\* #if USE_OMP *\/ */
+/* /\* #pragma omp for *\/ */
+/* /\* #endif *\/ */
+/* #pragma omp parallel for */
+/* 	for(i=0;i<10;i++) */
+/* 		printf("Hello from thread %d\n", omp_get_thread_num()); */
+
+
+
+/* TRY PARALLEL WITH COMMON OUTPU ARRAY */
+	#define VECSIZE 100
+	int i, j, k, temp, counter, *vec1, *vec2, I=5, K=5;
+	vec1 = (int*) calloc(VECSIZE, sizeof(int));
+	vec2 = (int*) calloc(VECSIZE, sizeof(int));
+
+	time_t time1,time2;
+
+	printf("\n== Serial for loop ==\n");
+	time(&time1);
+
+	counter = 0;
+	for(k=0;k<K;k++){
+		for(i=0;i<I;i++){
+			vec1[counter] = counter;
+			counter++;
+			j=0;
+			while(j<100e6) j++;
+		}
+	}
+
+	time(&time2);
+	printf("\ntime ellapsed: %d seconds \n", (int) (time2-time1));
+
+
+	printf("\n== Parallel for loop ==\n");
+	time(&time1);
+
+	counter = 0;
+//#pragma omp parallel for private(i,temp,j)
+	for(k=0;k<K;k++){
+#pragma omp parallel for private(temp,j)
+		for(i=0;i<I;i++){
+			temp = (k*I) + i;
+			vec2[temp] = temp;
+			j=0;
+			while(j<100e6) j++;
+		}
+	}
+
+	time(&time2);
+	printf("\ntime ellapsed: %d seconds \n", (int) (time2-time1));
+
+
+	printf("\nArray (serial):\n");
+	for(i=0;i<(I*K);i++){
+		printf("%d ", vec1[i]);
+	}
+	printf("\n");
+
+	printf("\nArray (parallel):\n");
+	for(i=0;i<(I*K);i++){
+		printf("%d ", vec2[i]);
+	}
+	printf("\n");
+
+	for(i=1;i<0;i++) printf("!%d ",i);
+
+	free(vec1);
+	free(vec2);
+	return 0;
+
+}
